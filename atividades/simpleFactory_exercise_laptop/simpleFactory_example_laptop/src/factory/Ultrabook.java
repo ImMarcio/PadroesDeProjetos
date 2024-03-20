9a67d4ef8463062035a472a9e6b8ace7f4634372
ImMarcio <marciojsilva159@gmail.com>
2024-03-19 21:53:36 -0300

    Nova atividade. Implementando o padrão "null object"

---
+++ atividades/simpleFactory_exercise_laptop/simpleFactory_example_laptop/src/factory/Ultrabook.java
@@ -0,0 +1,18 @@
+package factory;
+
+public class Ultrabook implements Laptop {
+
+	@Override
+	public void runTests() {
+		System.out.println("Executando os tetes em um Ultrabook Laptop");
+
+	}
+
+	@Override
+	public void assemble(String model, String cpu, int ram, int hd) {
+		System.out.println("Montando Laptop " + getClass().getSimpleName() + " model " + model + " " + ram + "GB " + hd + " GB");
+
+		
+	}
+
+}
